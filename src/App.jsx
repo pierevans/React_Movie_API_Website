0252ca62b9c3e38e62140b6cfcb9fe4d37967a0b
pik8014 <pierre.konan@mavs.uta.edu>
2026-06-05 02:45:34 -0500

    movie card is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
+import { CiHeart } from "react-icons/ci";
 
-function App() {
-  const [count, setCount] = useState(0)
+const MovieCard = ({ movie }) => {
+  const [like, setLike] = useState();
+  let clickOnCard = () => {
+    setLike(!like);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="movie-card">
+      <div className="movie-poster">
+        <img src={movie.url} alt={movie.title} />
+        <div className="movie-overlay">
+            <button className="favorite-btn" onClick={clickOnCard}>
+            {like ? (
+                <CiHeart style={{ color: "white" }} />
+            ) : (
+                <CiHeart style={{ color: "red" }} />
+            )}
+            </button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="movie-info">
+        <h3>{movie.title}</h3>
+        <p>{movie.release_date}</p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default App
+export default MovieCard;
